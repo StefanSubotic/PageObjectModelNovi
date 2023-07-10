b910c22b7fa36aa091dab4e3171676c190a2f44c
Stefan Subotic <stefan.subotic91@gmail.com>
2023-07-10 22:12:12 +0200

    AddRemoveProduct

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -97,6 +97,19 @@ public class InventoryTest extends BaseTest{
         Assert.assertEquals(checkoutStepOnePage.getError(), "Error: First Name is required");
 
     }
+    @Test
+
+    public void RemoveProductFromCart()
+    {
+        inventoryPage.clickLight();
+        inventoryPage.clickBackpack();
+        inventoryPage.clickCart();
+        inventoryPage.removeBackpack();
+
+
+        Assert.assertEquals(inventoryPage.getCartNumber(), "1");
+
+    }
 
 
     @AfterMethod
